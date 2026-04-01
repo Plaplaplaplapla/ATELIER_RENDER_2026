3a1125ecedddedb96461b79dd834a288a8bfab61
Plaplaplaplapla <deprade-eliott@outlook.fr>
2026-04-01 15:21:33 +0200

    Add github_actor and env variables to Terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,3 +14,14 @@ variable "image_url" {
 variable "image_tag" {
   type = string
 }
+
+variable "github_actor" {
+  description = "GitHub username"
+  type        = string
+}
+
+variable "env" {
+  description = "Application environment"
+  type        = string
+  default     = "production"
+}
